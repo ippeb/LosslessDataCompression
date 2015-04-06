cd7953d7f4c5c898b1ba6df147713fd56e68a60c
Josef Ziegler <ippeb24@gmail.com>
2015-04-07 01:55:49 +0200

    Addded test script for LZW.

---
+++ LZW/test_LZW.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -v # Echoes all commands before executing.
+g++ -Wall -O2 LZW_encoder.cc -o lzw_encoder
+g++ -Wall -O2 LZW_decoder.cc -o lzw_decoder
+./lzw_encoder ../star_wars_episode_1.txt > /dev/null
+./lzw_decoder LZW_encoded.txt LZW_alphabet.txt > /dev/null
+DIFF=$(diff LZW_decoded.txt ../star_wars_episode_1.txt)
+set +v # Command echoing off.
+
+if [ "$DIFF" != "" ] 
+then
+    echo "FALSE - LZW_decoded.txt ../star_wars_episode_1.txt"
+else
+    echo "CORRECT"
+fi
