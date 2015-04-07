3603f7e83d0fae98f447b9f1daf1313e8e1c6957
Josef Ziegler <ippeb24@gmail.com>
2015-04-07 02:05:21 +0200

    LZW: Remove echoing in test script.

--- LZW/test_LZW.sh
+++ LZW/test_LZW.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -v # Echoes all commands before executing.
+# set -v # Echoes all commands before executing.
 g++ -Wall -O2 LZW_encoder.cc -o lzw_encoder
 g++ -Wall -O2 LZW_decoder.cc -o lzw_decoder
 ./lzw_encoder ../star_wars_episode_1.txt > /dev/null
 ./lzw_decoder LZW_encoded.txt LZW_alphabet.txt > /dev/null
 DIFF=$(diff LZW_decoded.txt ../star_wars_episode_1.txt)
-set +v # Command echoing off.
+# set +v # Command echoing off.
 
 if [ "$DIFF" != "" ] 
 then
