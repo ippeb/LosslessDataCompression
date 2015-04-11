448fb745de278295dcbdde774416619068e4c8f4
Josef Ziegler <ippeb24@gmail.com>
2015-04-11 21:13:46 +0200

    RLE: Improved tester script.

--- RLE/test_RLE.sh
+++ RLE/test_RLE.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-# set -v # Echoes all commands before executing.
-g++ -Wall -O2 RLE_encoder.cc -o rle_encoder
-g++ -Wall -O2 RLE_decoder.cc -o rle_decoder
-./rle_encoder ../samples/zero_one_sample.txt > /dev/null
-./rle_decoder RLE_encoded.txt > /dev/null
-DIFF=$(diff RLE_decoded.txt ../samples/zero_one_sample.txt)
-# set +v # Command echoing off.
+make > /dev/null
 
-if [ "$DIFF" != "" ] 
-then
-    echo "FALSE"
-else
-    echo "CORRECT"
-fi
+array=("single_one.txt" "single_zero.txt" "zero_one_sample.txt")
+for i in "${array[@]}"
+do 
+    ./rle_encoder "../samples/"$i > /dev/null
+    ./rle_decoder "RLE_encoded.txt" > /dev/null
 
-rm rle_encoder rle_decoder RLE_encoded.txt RLE_decoded.txt
+    DIFF=$(diff RLE_decoded.txt "../samples/"$i)
+    if [ -r RLE_decoded.txt ] && [ -r "../samples/"$i ] && [ "$DIFF" == "" ] 
+    then
+	echo $i "CORRECT"
+    else
+	echo $i "FALSE"
+    fi
+done
+
+make clean > /dev/null
