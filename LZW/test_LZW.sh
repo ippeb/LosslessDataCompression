b4d7a085a9fb2af561948e3c774a468ec4c3c907
Josef Ziegler <ippeb24@gmail.com>
2015-04-12 15:56:24 +0200

    LZW: test_LZW iterates through all samples.

--- LZW/test_LZW.sh
+++ LZW/test_LZW.sh
@@ -8,26 +8,24 @@
 
 make > /dev/null
 
-array=("single_one.txt" "single_zero.txt" "zero_one_sample.txt" 
-    "all_ascii_small.txt" "all_ascii_large.txt" "star_wars_episode_1.txt" 
-    "single_char_large.txt")
+array=(../samples/*)
 
-printf "%30s %10s %10s -> %10s\n" "file_name" "state" "test_size" "enc_size "
-echo "------------------------------------------------------------------"
+printf "%40s %10s %10s -> %10s\n" "file_name" "state" "test_size" "enc_size "
+echo "----------------------------------------------------------------------------"
 
 for i in "${array[@]}"
 do 
-    ./LZW_encoder "../samples/"$i > /dev/null
+    ./LZW_encoder $i > /dev/null
     ./LZW_decoder LZW_encoded.txt LZW_alphabet.txt > /dev/null
-    DIFF=$(diff LZW_decoded.txt "../samples/"$i)
+    DIFF=$(diff LZW_decoded.txt $i)
 
-    if [ -r LZW_decoded.txt ] && [ -r "../samples/"$i ] && [ "$DIFF" == "" ] 
+    if [ -r LZW_decoded.txt ] && [ -r $i ] && [ "$DIFF" == "" ] 
     then
-	originalsize=$(wc -c < "../samples/"$i)
+	originalsize=$(wc -c < $i)
 	encodedsize=$(wc -c < "LZW_encoded.txt")
-	printf "%30s %10s %10s -> %10s\n" $i "CORRECT" $originalsize $encodedsize
+	printf "%40s %10s %10s -> %10s\n" $i "CORRECT" $originalsize $encodedsize
     else
-	printf "%30s %10s %10s -> %10s\n" $i "FALSE" "N/A" "N/A"
+	printf "%40s %10s %10s -> %10s\n" $i "FALSE" "N/A" "N/A"
     fi
 done
 
