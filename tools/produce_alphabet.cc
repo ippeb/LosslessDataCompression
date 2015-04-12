5d47ed239580cc7d5e6097c8f104bb5ea9217f54
Josef Ziegler <ippeb24@gmail.com>
2015-04-12 15:00:58 +0200

    tools: Improved alphabet analyzer.

--- tools/produce_alphabet.cc
+++ tools/produce_alphabet.cc
@@ -1,5 +1,4 @@
 /* 
-
    A simple program that takes a file
    and produces a list containing its alphabet (a 
    string in which every distinct char in the 
@@ -8,18 +7,14 @@
    The main function takes as argument
    arg1: input file name, the alphabet of this file
          will be produced.
-   arg2: output file name.
-
 */
 
 #include <cstdio>
-#include <iostream>
 #include <cstring>
-#define INPUT_LIMIT 1000000
-
+#define INPUT_BUFFER_SIZE 10000000
 
 int main(int argc, char** argv) {
-  if (argc != 3) {
+  if (argc != 2) {
     fprintf(stderr, "Wrong number of arguments specified\n");
     return -1;
   }
@@ -31,33 +26,23 @@ int main(int argc, char** argv) {
     return -1;
   }
 
-  FILE *fout1 = fopen(argv[2], "w");
-
   // read input
-  char S[INPUT_LIMIT];
-  fread(S, sizeof(char), sizeof(S), fin1);
+  char* S = new char[INPUT_BUFFER_SIZE];
+  int slen = fread(S, sizeof(char), INPUT_BUFFER_SIZE, fin1);
 
   // mark which chars appeared
-  int slen = std::strlen(S);
-  bool B[256];
-  memset(B, 0, sizeof(B));
+  bool B[256] = { false };
   for (int i = 0; i < slen; i++) 
-    B[S[i]] = true;
+    B[(unsigned char) S[i]] = true;
 
   int count = 0;
   for (int i = 0; i < 256; i++) 
-    if (B[i]) {
-      fprintf(fout1, "%c", (char) i);
+    if (B[i])
       count++;
-    }
   
-  // debug info
   printf("Alphabet size: %d\n\n", count);
-  printf(">>>");
   for (int i = 0; i < 256; i++) 
     if (B[i]) 
       printf("%c", (char) i);
   printf("\n");
-
 }
-   
