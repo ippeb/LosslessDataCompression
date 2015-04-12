55f1ef4e6e7c5039e8606d41bb7aa27e659c358b
Josef Ziegler <ippeb24@gmail.com>
2015-04-12 14:48:21 +0200

    HUFFMAN: Improved test script.

--- HUFFMAN/test_HUFFMAN.sh
+++ HUFFMAN/test_HUFFMAN.sh
@@ -2,18 +2,26 @@
 make > /dev/null
 
 array=("single_one.txt" "single_zero.txt" "zero_one_sample.txt" 
-    "all_ascii_small.txt" "all_ascii_large.txt" "star_wars_episode_1.txt")
+    "all_ascii_small.txt" "all_ascii_large.txt" "star_wars_episode_1.txt" 
+    "single_char_large.txt")
+
+printf "%30s %10s %10s -> %10s (%10s)\n" "file_name" "state" "test_size" "enc_size" "mapping_size"
+echo "--------------------------------------------------------------------------------"
+
 for i in "${array[@]}"
 do 
     ./HUFFMAN_encoder "../samples/"$i > /dev/null
     ./HUFFMAN_decoder HUFFMAN_encoded.txt HUFFMAN_code_words_mapping.txt > /dev/null
-
     DIFF=$(diff HUFFMAN_decoded.txt "../samples/"$i)
+
     if [ -r HUFFMAN_decoded.txt ] && [ -r "../samples/"$i ] && [ "$DIFF" == "" ] 
     then
-	echo $i "CORRECT"
+	originalsize=$(wc -c < "../samples/"$i)
+	encodedsize=$(wc -c < "HUFFMAN_encoded.txt")
+	mappingsize=$(wc -c < "HUFFMAN_code_words.txt")
+	printf "%30s %10s %10s -> %10s (%10s)\n" $i "CORRECT" $originalsize $encodedsize $mappingsize
     else
-	echo $i "FALSE"
+	printf "%30s %10s %10s -> %10s (%10s)\n" $i "FALSE" "N/A" "N/A" "N/A"
     fi
 done
 
