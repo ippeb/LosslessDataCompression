c3ce20fd2b023750ee78c3a00f13dbdc982ea64e
Josef Ziegler <ippeb24@gmail.com>
2015-04-12 15:00:35 +0200

    tools: program to generate all ASCII input samples.

---
+++ tools/produce_all_ascii_sample.cc
@@ -0,0 +1,16 @@
+/*
+  Generate file containing all ASCII chars.
+
+ */
+
+#include <cstdio>
+using namespace std;
+
+int main() {
+  FILE *fout = fopen("all_ascii_large.txt", "w");
+  for (int j = 0; j < 4*4*256; j++) {
+    for (int i = 0; i < 256; i++) {
+      fprintf(fout, "%c", i);
+    }
+  }
+}
